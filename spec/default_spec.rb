ded6accb6bfb22f10a0905d418f68e7871f9a137
Jamie Winsor <jamie@vialstudios.com>
2014-08-04 19:29:47 -0700

    remove useless tests
    tests should not test wether the DSL is properly describing what we intended.
    This is what the DSL is for. Tests should check on disk to identify that the software
    was properly installed, ports are open, etc.

--- spec/default_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe 'elixir::default' do
-  let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
-
-  it 'includes elixir::source recipe' do
-    expect(chef_run).to include_recipe('elixir::source')
-  end
-end
