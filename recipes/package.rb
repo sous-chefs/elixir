bb6775b9f0e5bb17467998103e9327bca1fd241f
Jamie Winsor <jamie@vialstudios.com>
2014-08-04 19:28:39 -0700

    install elixir from github package

--- recipes/package.rb
+++ recipes/package.rb
@@ -6,6 +6,7 @@
 #
 
 node.normal[:erlang][:esl][:version] = "1:17.1"
+elixir_path = File.join(node[:elixir][:versions_path], node[:elixir][:version])
 
 include_recipe "apt::default"
 include_recipe "erlang::esl"
@@ -16,7 +17,15 @@ asset = github_asset "Precompiled.zip" do
   release node[:elixir][:source][:revision]
 end
 
+directory elixir_path do
+  recursive true
+end
+
 libarchive_file "Precompiled.zip" do
   path asset.asset_path
-  extract_to node[:elixir][:install_path]
+  extract_to elixir_path
+end
+
+link node[:elixir][:install_path] do
+  to elixir_path
 end
