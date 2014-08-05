8a5aa898b8fbf6690fcd183c54395aa4b2d16452
Jamie Winsor <jamie@vialstudios.com>
2014-08-04 19:04:15 -0700

    leverage erlang::esl recipe in package recipe

--- recipes/package.rb
+++ recipes/package.rb
@@ -5,16 +5,7 @@
 # Copyright (C) 2013-2014 Jamie Winsor (<jamie@vialstudios.com>)
 #
 
+node.normal[:erlang][:esl][:version] = "1:17.1"
+
 include_recipe "apt::default"
-
-apt_repository "erlang-solutions" do
-  uri "http://packages.erlang-solutions.com/ubuntu"
-  distribution node[:lsb][:codename]
-  components ["contrib"]
-  key "http://packages.erlang-solutions.com/ubuntu/erlang_solutions.asc"
-
-  action :add
-end
-
-package "erlang"
-package "elixir"
+include_recipe "erlang::esl"
