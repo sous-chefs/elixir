38d67eaf8b62a8306242912c9c5a78cda9633d36
Jamie Winsor <jamie@vialstudios.com>
2014-08-04 19:46:14 -0700

    symlink elixir binaries to /usr/bin

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,3 +7,12 @@
 
 ChefElixir.validate_attributes!(node)
 include_recipe "elixir::_#{node[:elixir][:install_method]}"
+
+bin_path    = File.join(node[:elixir][:install_path], "bin")
+executables = ["elixir", "elixirc", "iex", "mix"]
+
+executables.each do |executable|
+  link "/usr/bin/#{executable}" do
+    to File.join(bin_path, executable)
+  end
+end
