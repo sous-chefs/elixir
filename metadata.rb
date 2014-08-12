a7a5bc95c0b76426eae8cef5a07726061cc1831a
Jamie Winsor <jamie@vialstudios.com>
2014-08-11 17:38:36 -0700

    version bump 0.8.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "jamie@vialstudios.com"
 license          "Apache 2.0"
 description      "Manages an Elixir installation"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version          "0.8.1"
+version          "0.8.2"
 
 depends "apt"
 depends "git"
