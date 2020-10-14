df9c1e2c6421213d91873dd43052c6ca1a6ed272
Lance Albertson <lance@osuosl.org>
2020-10-14 12:08:49 -0700

    Update to Elixir 1.11.0
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,7 +5,7 @@
 # Copyright:: 2013-2019, Jamie Winsor (<jamie@vialstudios.com>)
 #
 
-default['elixir']['version']        = '1.4.2'
+default['elixir']['version']        = '1.11.0'
 default['elixir']['install_method'] = 'package'
 default['elixir']['install_path']   = '/usr/local/lib/elixir'
 
